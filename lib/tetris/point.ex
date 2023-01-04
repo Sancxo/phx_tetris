b518a6d96a3ec9ec0e0b1eb51f0b24a2a03ffea5
Simon <simon@7lieues.io>
2023-01-04 17:39:06 +0100

    docs: change x axis type and improves typedoc for x and y axis

--- lib/tetris/point.ex
+++ lib/tetris/point.ex
@@ -1,8 +1,8 @@
 defmodule Tetris.Point do
   # Types:
-  @typedoc "The X axis (left/right) of the point."
-  @type x() :: number()
-  @typedoc "The Y axis (up/down) of the point. In Tetris we can't go up, so no negative numbers"
+  @typedoc "The X axis (left/right) of the point, where 0 is the left extremity of the space."
+  @type x() :: non_neg_integer()
+  @typedoc "The Y axis (up/down) of the point, where 0 is the top extremity of the space."
   @type y() :: non_neg_integer()
   @typedoc "Represents the position of a point as a tuple of x and y coordinates."
   @type point(x, y) :: {x, y}
