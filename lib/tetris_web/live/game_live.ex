e1ae53b288e922c1bf8f58eb33766138a75ac17d
Simon <simon@7lieues.io>
2023-01-05 17:26:43 +0100

    feat(GameLive): creates a tetromino which moves down in LiveView

--- lib/tetris_web/live/game_live.ex
+++ lib/tetris_web/live/game_live.ex
@@ -1,24 +1,33 @@
 defmodule TetrisWeb.GameLive do
+  alias Tetris.Tetromino
   use TetrisWeb, :live_view
 
   @impl true
   def mount(_params, _session, socket) do
     :timer.send_interval(500, :tick)
-    {:ok, socket |> new_game()}
-  end
-
-  def new_game(socket) do
-    socket
+    {:ok, socket |> new_tetromino()}
   end
 
   @impl true
-  def handle_info(:tick, socket) do
-    {:noreply, socket}
-  end
+  def handle_info(:tick, socket), do: {:noreply, socket |> tetro_down()}
 
   @impl true
   def render(assigns) do
     ~H"""
+    <%= {x, y} = @tetro.location %>
+    <section class="phx-hero">
+      <pre>
+      shape: <%= @tetro.shape |> inspect() %>
+      rotation: <%= @tetro.rotation |> inspect() %>
+      location: {<%= x %>, <%= y %>}
+      </pre>
+    </section>
     """
   end
+
+  # PRIVATES
+  defp new_tetromino(socket), do: socket |> assign(tetro: Tetromino.new_random())
+
+  defp tetro_down(%{assigns: %{tetro: tetro}} = socket),
+    do: socket |> assign(tetro: tetro |> Tetromino.down())
 end
