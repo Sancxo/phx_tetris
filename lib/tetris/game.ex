d58c4bbf5b2868236bf824639e6a15ba1fc5b42d
Simon <simon@7lieues.io>
2023-01-11 21:30:57 +0100

    feat: connects the ability to move with the boundaries

--- lib/tetris/game.ex
+++ lib/tetris/game.ex
@@ -1,15 +1,28 @@
 defmodule Tetris.Game do
   alias Tetris.{Tetromino, Points}
 
-  defstruct [:tetro, score: 0, junkyard: %{}]
+  defstruct [:tetro, points: [], score: 0, junkyard: %{}]
+
+  def new(), do: __struct__() |> new_tetromino() |> show()
+
+  def new_tetromino(game), do: %{game | tetro: Tetromino.new_random()} |> show()
+
+  def show(game), do: %{game | points: game.tetro |> Tetromino.show()}
 
   def move(game, move_fn) do
     old = game.tetro
     new = game.tetro |> move_fn.()
 
-    new
-    |> Tetromino.show()
-    |> Points.valid?()
-    |> Tetromino.maybe_move(old, new)
+    tetro =
+      new
+      |> Tetromino.show()
+      |> Points.valid?()
+      |> Tetromino.maybe_move(old, new)
+
+    %{game | tetro: tetro} |> show()
   end
+
+  def right(game), do: game |> move(&Tetromino.right/1)
+  def left(game), do: game |> move(&Tetromino.left/1)
+  def rotate(game), do: game |> move(&Tetromino.rotate/1)
 end
